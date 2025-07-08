b02cfdcb382db35c0418aa3d6d1a7a4c85a98307
Abdulrahman Elnory <135131957+abdulrahmanelnory1@users.noreply.github.com>
2025-07-08 22:56:40 +0300

    Update PaperBook.java

--- src/PaperBook.java
+++ src/PaperBook.java
@@ -3,30 +3,13 @@ public class PaperBook extends Book {
 
     /** the number of available physical copies of the book in the inventory. */
     private int stock;
-    private String isbn;
-    private String title;
-    private String year;
-    private double price;
+
 
     public  PaperBook(String isbn, String title, String year, double price, int stock) {
         super(isbn, title, year, price);
         this.stock = stock;
     }
 
-    public String getIsbn(){
-        return isbn;
-    }
-
-    public String getTitle(){
-        return title;
-    }
-    public String getYear(){
-        return year;
-    }
-
-    public double getPrice(){
-        return price;
-    }
 
     public int getStock(){
         return stock;
